f3ebbc7cedaa714e6a25e8d08d985b41b760af23
ChadDiaz <chadcdiaz@gmail.com>
2021-01-28 00:01:16 -0500

    took styling out for now

--- the-reducer-pattern/src/components/Todo.js
+++ the-reducer-pattern/src/components/Todo.js
@@ -2,15 +2,12 @@ import React from "react";
 
 const Todo = ({ todo, id, completeTodo, removeTodo }) => {
   return (
-    <div
-    className="todo"
-    style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
-  >
-    {todo.itemName}
+    <div style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+      {todo.itemName}
 
       <div>
-          <button onClick={() => completeTodo(id)}>Complete</button>
-          <button onClick={() => removeTodo(id)}>Remove</button>
+        <button onClick={() => completeTodo(id)}>Complete</button>
+        <button onClick={() => removeTodo(id)}>Remove</button>
       </div>
     </div>
   );
